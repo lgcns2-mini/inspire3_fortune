f3a1f2d6a4c0e684f487fecbd4eeb1c2c5bdc885
aaahyunseo <hs030608@naver.com>
2025-10-23 10:08:50 +0900

    init : fortune ai initialization

---
+++ src/main/java/com/lgcns/inspire3_fortune/Inspire3FortuneApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_fortune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inspire3FortuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inspire3FortuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/inspire3_fortune/Inspire3FortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_fortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inspire3FortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
